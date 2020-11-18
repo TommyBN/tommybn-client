90c4beaaa0c4913d3af6a80ebfa03e80fb91cabc
Tom Ben Ner <tommybenner@hotmail.com>
2020-11-18 15:46:13 +0200

    styling styling styling....

--- src/app/welcome/main/main.component.ts
+++ src/app/welcome/main/main.component.ts
@@ -7,11 +7,9 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  @ViewChild('image') image: ElementRef;  
-  @ViewChild('imageText') imageText: ElementRef;  
-  @ViewChild('about') aboutElement: ElementRef;  
-  @ViewChild('apps') appsElement: ElementRef;  
-  @ViewChild('whatDo') whatDoElement: ElementRef;  
+  @ViewChild('image') image: ElementRef;
+  @ViewChild('imageText') imageText: ElementRef;
+
   name: string;
   about: boolean = false;
   apps: boolean = false;
@@ -20,12 +18,12 @@ export class MainComponent implements OnInit {
   // <h1>heading</h1>
   // <p>bla bla bla bla </p>
   // `
-  
+
 
   constructor(private elementRef: ElementRef) { }
 
-  ngOnInit() {   }
-  
+  ngOnInit() { }
+
   ngAfterViewInit() {
     // console.log(this.imageText.nativeElement.innerHTML = `
     // <h1>heading</h1>
@@ -39,58 +37,52 @@ export class MainComponent implements OnInit {
   }
 
   toggleDiv(divName: string) {
-    if(screen.width > 768 || screen.width < 600) {
-      switch (divName) {
-        case 'about' : 
+    switch (divName) {
+      case 'about':
         this.about = !this.about;
         this.apps = false;
         this.techs = false;
+        if (screen.width < 768 && screen.width > 600) {
+          this.imageText.nativeElement.style.opacity = this.about ? 1 : 0;
+          this.image.nativeElement.style.opacity = this.about ? 0.3 : 1;
+        }
         break;
-        case 'apps' : 
+      case 'apps':
         this.about = false;
         this.apps = !this.apps;
         this.techs = false;
+        if (screen.width < 768 && screen.width > 600) {
+          this.imageText.nativeElement.style.opacity = this.apps ? 1 : 0;
+          this.image.nativeElement.style.opacity = this.apps ? 0.3 : 1;
+        }
         break;
-        case 'techs' : 
+      case 'techs':
         this.about = false;
         this.apps = false;
         this.techs = !this.techs;
+        if (screen.width < 768 && screen.width > 600) {
+          this.imageText.nativeElement.style.opacity = this.techs ? 1 : 0;
+          this.image.nativeElement.style.opacity = this.techs ? 0.3 : 1;
+        }
         break;
-        
-      }
-    }
-    else {
-      this.image.nativeElement.style.opacity = 0.3;
-      switch (divName) {
-        case 'about':
-          this.imageText.nativeElement.innerHTML = this.aboutElement.nativeElement.innerHTML;
-          this.imageText.nativeElement.style.opacity = 1;
-          break;
-        case 'apps':
-          this.imageText.nativeElement.innerHTML = this.appsElement.nativeElement.innerHTML;
-          this.imageText.nativeElement.style.opacity = 1;
-          break;
-        case 'techs':
-          this.imageText.nativeElement.innerHTML = this.whatDoElement.nativeElement.innerHTML;
-          this.imageText.nativeElement.style.opacity = 1;
-          break;
-      }
+
     }
   }
+
 
   setHeight(divName: string) {
     switch (divName) {
       case 'about':
-        if(screen.width > 760) return this.about ? '170px' : '0px';
-        if(screen.width > 600) return this.about ? '100px' : '0px'
-        return this.about ? '332px' : '0px'
+        if (screen.width > 760) return this.about ? '170px' : '0px';
+        if (screen.width > 600) return '0px'
+        return this.about ? '200px' : '0px'
       case 'apps':
-        if(screen.width > 760) return this.apps ? '291px' : '0px'
-        if(screen.width > 600) return this.apps ? '200px' : '0px'
+        if (screen.width > 760) return this.apps ? '291px' : '0px'
+        if (screen.width > 600) return '0px'
         return this.apps ? '400px' : '0px'
       case 'techs':
-        if(screen.width > 760) return this.techs ? '500px' : '0px'
-        if(screen.width > 600) return this.techs ? '300px' : '0px'
+        if (screen.width > 760) return this.techs ? '500px' : '0px'
+        if (screen.width > 600) return '0px'
         return this.techs ? '400px' : '0px'
     }
   }
